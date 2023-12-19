2141ba781d4e2fba37c2e4b6a8874072e09284e3
adil maarad <76706412+adilmaarad@users.noreply.github.com>
2023-12-19 16:58:05 -0100

    first commit

---
+++ src/components/NumberCounter.jsx
@@ -0,0 +1,27 @@
+// NumberCounter.js
+import React, { useState } from 'react';
+import './NumberCounter.css';
+
+const NumberCounter = () => {
+  const [number, setNumber] = useState(0);
+
+  const incrementNumber = () => {
+    setNumber(number + 1);
+  };
+
+  const decrementNumber = () => {
+    setNumber(number - 1);
+  };
+
+  return (
+    <div className={`number-container ${number > 0 ? 'blue' : number < 0 ? 'red' : ''}`}>
+      <p className='number'>{number}</p>
+      <div className="button-container">
+        <button className="decrement" onClick={decrementNumber}>-</button>
+        <button className="increment" onClick={incrementNumber}>+</button>
+      </div>
+    </div>
+  );
+};
+
+export default NumberCounter;
